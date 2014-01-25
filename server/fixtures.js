788153ed0a5012ce6adb131007c7fc7aecb0c2ea
William Oliveira <sudowilliam@gmail.com>
2014-01-25 15:22:48 -0200

    adds fixtures.js

---
+++ server/fixtures.js
@@ -0,0 +1,19 @@
+if (Posts.find().count() === 0) {
+  Posts.insert({
+    title: 'Introducing Telescope',
+    author: 'Sacha Greif',
+    url: 'http://sachagreif.com/introducing-telescope'
+  });
+
+  Posts.insert({
+    title: 'Testando essa app',
+    author: 'William Oliveira',
+    url: 'http://github.com/gnuwilliam'
+  });
+
+  Posts.insert({
+    title: 'Luke, Im your father!',
+    author: 'Darth Vader',
+    url: 'http://starwars.com'
+  });
+}
