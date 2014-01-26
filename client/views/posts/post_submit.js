368008ac9020b7f6628c29f28937c2f012cbc48a
William Oliveira <sudowilliam@gmail.com>
2014-01-26 21:01:52 -0200

    JSHint

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -6,7 +6,7 @@ Template.postSubmit.events({
       url: $(event.target).find('[name=url]').val(),
       title: $(event.target).find('[name=title]').val(),
       message: $(event.target).find('[name=message]').val()
-    }
+    };
 
     post._id = Posts.insert(post);
     Meteor.Router.to('postPage', post);
