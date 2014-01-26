dd3e776adf4b1d0d5d43fd1b824c13956f863ba7
William Oliveira <sudowilliam@gmail.com>
2014-01-26 21:00:49 -0200

    implementing post submit event

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,3 +1,11 @@
 Template.postSubmit.events({
+  'submit form': function (event) {
+    event.preventDefault();
 
+    var post = {
+      url: $(event.target).find('[name=url]').val(),
+      title: $(event.target).find('[name=title]').val(),
+      message: $(event.target).find('[name=message]').val()
+    }
+  }
 });
