3ae3dd458cac213a9e658aba752d389b4a48b74c
William Oliveira <sudowilliam@gmail.com>
2014-01-27 02:29:26 -0200

    error helper

--- client/views/includes/errors.js
+++ client/views/includes/errors.js
@@ -3,3 +3,11 @@ Template.errors.helpers({
     return Errors.find();
   }
 });
+
+Template.error.rendered = function() {
+  var error = this.data;
+
+  Meteor.defer(function () {
+    Errors.update(error._id, { $set: { seen: true } });
+  });
+};
