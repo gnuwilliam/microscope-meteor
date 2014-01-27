9f16047b86298aed179c1188605a009c90960be6
William Oliveira <sudowilliam@gmail.com>
2014-01-27 03:25:21 -0200

    removing error package from main repo

--- packages/errors/README.md
+++
@@ -1,2 +0,0 @@
-# Meteor Simple Errors
-A pattern to display application errors to the user
--- packages/errors/errors_tests.js
+++
@@ -1,8 +0,0 @@
-Tinytest.add("Errors collection works", function(test) {
-  test.equal(Meteor.errors.find({}).count(), 0);
-  
-  Meteor.Errors.throw('A new error!');
-  test.equal(Meteor.errors.find({}).count(), 1);
-  
-  Meteor.errors.remove({});
-});
