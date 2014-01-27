a9550565dcb370b4793f00d19e5e45605b23c931
William Oliveira <sudowilliam@gmail.com>
2014-01-27 01:17:36 -0200

    post update method

--- client/views/posts/post_edit.js
+++ client/views/posts/post_edit.js
@@ -14,5 +14,14 @@ Template.postEdit.events({
       url: $(event.target).find('[name=url]').val(),
       title: $(event.target).find('[name=title]').val()
     }
+
+    Posts.update(currentPostId, { $set: postProperties }, function (error) {
+      if (error) {
+        // display the error to the user
+        alert(error.reason);
+      } else {
+        Meteor.Router.to('postPage', currentPostId);
+      }
+    });
   }
 });
