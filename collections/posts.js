96fdb7f712500f3a23f7eee60bda4ad32c8b4bce
William Oliveira <sudowilliam@gmail.com>
2014-01-27 00:13:31 -0200

    adds Meteor.methods in post collection

--- collections/posts.js
+++ collections/posts.js
@@ -1,5 +1,17 @@
 Posts = new Meteor.Collection('posts');
 
+Meteor.methods({
+  post: function (postAttributes) {
+    var user = Meteor.user();
+    var postWithSameLink = Posts.findOne({ url: postAttributes.url });
+
+    // ensure the user is logged in
+    if (!user) {
+      throw new Meteor.error(401, 'You need to login to post new stories');
+    }
+  }
+});
+
 Posts.allow({
   insert: function (userId, doc) {
     // only allow posting if you are logged in
