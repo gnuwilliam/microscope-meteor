923d83ba47d6d847b3920eaa5b60ee111c11a0e6
William Oliveira <sudowilliam@gmail.com>
2014-01-27 02:39:46 -0200

    implements clear method

--- packages/errors/errors.js
+++ packages/errors/errors.js
@@ -4,5 +4,9 @@ Meteor.errors = new Meteor.Collection(null);
 Meteor.errors = {
   throw: function (message) {
     Meteor.errors.insert({ message: message, seen: false });
+  },
+
+  clear: function () {
+    Meteor.errors.remove({ seen: true });
   }
 }
