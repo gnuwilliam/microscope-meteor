3052005eac226c9db54771148e2dc84636047c36
William Oliveira <sudowilliam@gmail.com>
2014-01-27 01:30:35 -0200

    post deny method

--- collections/posts.js
+++ collections/posts.js
@@ -5,6 +5,13 @@ Posts.allow({
   remove: ownsDocument
 });
 
+Posts.deny({
+  update: function (userId, post, fieldNames) {
+    // may only edit the following three fields
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
+
 Meteor.methods({
   post: function (postAttributes) {
     var user = Meteor.user();
