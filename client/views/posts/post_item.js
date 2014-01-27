20630f026ddd39d55230e161eac73442cee9ea06
William Oliveira <sudowilliam@gmail.com>
2014-01-27 01:24:33 -0200

    ownPost helper

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,4 +1,8 @@
 Template.postItem.helpers({
+  ownPost: function () {
+    return this.userId == Meteor.userId();
+  },
+
   domain: function () {
     var a = document.createElement('a');
     a.href = this.url;
