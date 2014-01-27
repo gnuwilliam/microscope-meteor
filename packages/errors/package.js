2c647164b91f3b567d8accf75b8873d885a425ed
William Oliveira <sudowilliam@gmail.com>
2014-01-27 02:56:07 -0200

    package test suite

--- packages/errors/package.js
+++ packages/errors/package.js
@@ -6,3 +6,10 @@ Package.on_use(function (api, where) {
   api.use(['minimongo', 'mongo-livedata', 'templating'], 'client');
   api.add_files(['errors.js', 'errors_list.html', 'errors_list.html'], 'client');
 });
+
+Package.on_test(function (api) {
+  api.use('errors', 'client');
+  api.use(['tinytest', 'test-helpers'], 'client');
+
+  api.add_files('errors_tests.js', 'client');
+});
