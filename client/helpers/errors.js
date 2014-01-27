c8ae8d07b744794098a21092ac6461d89dc31405
William Oliveira <sudowilliam@gmail.com>
2014-01-27 02:24:37 -0200

    clearErrors method

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -4,3 +4,7 @@ Errors = new Meteor.Collection(null);
 throwError = function (message) {
   Errors.insert({ message: message, seen: false });
 };
+
+clearErrors = function () {
+  Errors.remove({ seen: true });
+}
