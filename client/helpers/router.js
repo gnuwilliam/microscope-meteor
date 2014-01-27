5f9d934b3bd57e1c15bee0a41ca1321c11c35b56
William Oliveira <sudowilliam@gmail.com>
2014-01-26 23:01:13 -0200

    define router filter
    prevent logged out users to see the post submit form

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -8,3 +8,13 @@ Meteor.Router.add({
 
   '/submit': 'postSubmit'
 });
+
+Meteor.Router.filters({
+  'requireLogin': function (page) {
+    if (Meteor.user()) {
+      return page;
+    } else {
+      return 'accessDenied';
+    }
+  }
+});
