e88cb5688b67fd7f4d6dfa04e50592a1324faa63
William Oliveira <sudowilliam@gmail.com>
2014-01-27 02:47:23 -0200

    post_edit new error api

--- client/views/posts/post_edit.js
+++ client/views/posts/post_edit.js
@@ -18,7 +18,7 @@ Template.postEdit.events({
     Posts.update(currentPostId, { $set: postProperties }, function (error) {
       if (error) {
         // display the error to the user
-        alert(error.reason);
+        Meteor.Errors.throw(error.reason);
       } else {
         Meteor.Router.to('postPage', currentPostId);
       }
