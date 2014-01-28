974a38b5d6234c9f2bd2332a1e9aed0f5c05364e
William Oliveira <sudowilliam@gmail.com>
2014-01-28 02:28:02 -0200

    define comments methods

--- collections/comments.js
+++ collections/comments.js
@@ -1,1 +1,9 @@
 Comments = new Meteor.Collection('comments');
+
+Meteor.methods({
+  comment: function (commentAttributes) {
+    var user = Meteor.user();
+    var post = Posts.findOne(commentAttributes.postId);
+
+  }
+});
