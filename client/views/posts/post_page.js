2d337ba949a866fe20ce925fa81da9c67904e2dc
William Oliveira <sudowilliam@gmail.com>
2014-01-28 01:46:22 -0200

    comments helper

--- client/views/posts/post_page.js
+++ client/views/posts/post_page.js
@@ -1,5 +1,9 @@
 Template.postPage.helpers({
   currentPost: function () {
     return Posts.findOne(Session.get('currentPostId'));
+  },
+
+  comments: function () {
+    return Comments.find({ postId: this._id });
   }
 });
