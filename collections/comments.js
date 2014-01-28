08fcf7f5c4db46d88b0118df0a484893f8948cbf
William Oliveira <sudowilliam@gmail.com>
2014-01-28 02:28:31 -0200

    ensure the user is logged in

--- collections/comments.js
+++ collections/comments.js
@@ -4,6 +4,10 @@ Meteor.methods({
   comment: function (commentAttributes) {
     var user = Meteor.user();
     var post = Posts.findOne(commentAttributes.postId);
-
+  
+    // ensure the user is logged in
+    if (!user) {
+      throw new Meteor.Error(401, "You need to login to make comments");
+    }
   }
 });
