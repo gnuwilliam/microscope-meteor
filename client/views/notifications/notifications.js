3cc6e0e1888598a1d270b9c5058099a187c1a763
William Oliveira <sudowilliam@gmail.com>
2014-01-29 03:26:11 -0200

    notifications helpers

---
+++ client/views/notifications/notifications.js
@@ -0,0 +1,10 @@
+Template.notifications.helpers({ 
+  notifications: function () {
+    return Notifications.find({ userId: Meteor.userId(), read: false }); 
+  },
+
+  notificationCount: function () {
+    return Notifications.find({ userId: Meteor.userId(), read: false }).count();
+  }
+});
+
